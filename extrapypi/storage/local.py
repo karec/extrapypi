d5e273dd0aa6ac36cf5bc2f4013f2f1284b384e0
karec <manu.valette@gmail.com>
2017-12-27 12:05:54 +0100

    update regex to find files

--- extrapypi/storage/local.py
+++ extrapypi/storage/local.py
@@ -66,7 +66,7 @@ class LocalStorage(BaseStorage):
             return False
 
         files = os.listdir(path)
-        regex = '{}-(?P<version>[0-9\.]*)[\.-].*'.format(package.name)
+        regex = '.*-(?P<version>[0-9\.]*)[\.-].*'
         r = re.compile(regex)
         files = filter(
             lambda f: r.match(f) and r.match(f).group('version') == version,
@@ -118,7 +118,7 @@ class LocalStorage(BaseStorage):
 
         files = os.listdir(path)
         if release is not None:
-            regex = '{}-(?P<version>[0-9\.]*)[\.-].*'.format(package.name)
+            regex = '.*-(?P<version>[0-9\.]*)[\.-].*'.format(package.name)
             r = re.compile(regex)
             v = release.version
             files = filter(
