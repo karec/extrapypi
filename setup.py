9aa0bcb6ec2c924c7cae36debad6d133a2e2eab5
karec <manu.valette@gmail.com>
2018-02-02 16:43:14 +0100

    bump version to 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 ROOT = os.path.realpath(os.path.join(os.path.dirname(
     sys.modules['__main__'].__file__)))
 
-__version__ = '0.2'
+__version__ = '0.3'
 
 
 setup(
